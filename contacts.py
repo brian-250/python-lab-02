77ea94e5e059d5463e326775326a9824c3926098
Brian Milian <brianmilian@csu.fullerton.edu>
2024-09-09 14:32:55 -0700

    Add 'print_list()' function

--- contacts.py
+++ contacts.py
@@ -1,3 +1,27 @@
 # Name: Brian Milian
 # Date: 9/9/24
 # File Purpose: Tuffy Titan's Contacts List Manipulation
+
+contacts = []
+
+def print_list(contacts):
+    """
+    Prints each list in 'contacts' on a new line.
+
+    Paramters:
+    contacts (2D list): The only list in the parameter.
+
+    Methods:
+    print():
+        Prints a title for Tuffy Titan's 'contacts' list.
+    print():
+        Prints a header for the index number, first name, and last name of
+        each list in 'contacts'.
+    print():
+        Prints each list in 'contacts' in the chronilogical format
+        of: index number, first name, and last name.
+    """
+    print("------------TUFFY TITAN'S CONTACTS LIST-------------")
+    print("#-------First Name------------Last Name-------------")
+    for i in contacts:
+        print(f'{str(i):8}{contacts[i][0]:22}{contacts[i][1]:22}')
