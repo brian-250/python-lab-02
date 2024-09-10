58feb5345cbe18fe701d19e561840a805193afc0
Brian Milian <brianmilian@csu.fullerton.edu>
2024-09-10 00:17:12 -0700

    Fix first if statement conditional

--- contacts.py
+++ contacts.py
@@ -78,7 +78,7 @@ def modify_contact(contacts):
     contacts
     """
     index_modifier = int(input("What index do you want to modify? "))
-    if index_modifier >= min(contacts) and index_modifier <= max(contacts):
+    if index_modifier >= 0 and index_modifier < len(contacts):
         first_name = input("Provide your first name: ")
         last_name = input("Provide your last name: ")
         full_name = []
